c714794b5adb97cea4aa7ec272d5f808610bbc1e
sisappmobile <sisappmobile@gmail.com>
2025-01-17 16:54:57 +0700

    Update

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -6,22 +6,24 @@ extension ColorExtension on Color {
   Color darken([int percent = 10]) {
     assert(1 <= percent && percent <= 100);
     var f = 1 - percent / 100;
-    return Color.fromARGB(
-        a.toInt(),
-        (r.toInt() * f).round(),
-        (g.toInt()  * f).round(),
-        (b.toInt() * f).round(),
+    return Color.from(
+        alpha: a,
+        red: r * f,
+        green: g  * f,
+        blue: b * f,
     );
   }
 
   Color lighten([int percent = 10]) {
     assert(1 <= percent && percent <= 100);
+
     var p = percent / 100;
-    return Color.fromARGB(
-        a.toInt(),
-        r.toInt() + ((255 - r.toInt()) * p).round(),
-        g.toInt() + ((255 - g.toInt()) * p).round(),
-        b.toInt() + ((255 - b.toInt()) * p).round(),
+
+    return Color.from(
+        alpha: a,
+        red: r + ((255 - r) * p),
+        green: g + ((255 - g) * p),
+        blue: b + ((255 - b) * p),
     );
   }
 }
