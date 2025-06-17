34e216ddedee62daa7208a2709ff85bbfbb19969
sisappmobile <sisappmobile@gmail.com>
2025-06-17 22:38:25 +0700

    Update

--- lib/src/page/base_checkable_page.dart
+++ lib/src/page/base_checkable_page.dart
@@ -79,6 +79,7 @@ class BaseCheckablePageState extends State<BaseCheckablePage> with WidgetsBindin
         ],
       ),
       contentBuilder: body,
+      bottomNavigationBar: bottomBar(),
     );
   }
 
